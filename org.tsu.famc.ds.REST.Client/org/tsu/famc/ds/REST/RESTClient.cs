e666c4bc2d8b679a9e0dd54d4ec89196cb8d82cc
Алексей Мирютов <a.mirutov@hotmail.com>
2016-03-25 22:17:54 +0600

    Пример REST клиента и сервера

---
+++ org.tsu.famc.ds.REST.Client/org/tsu/famc/ds/REST/RESTClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ProductStoreClient
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Array
+    {
+        public int[] data;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ArraySampleTest().Wait();
+            BookSampleGetTest().Wait();
+            BookSamplePostTest().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task ArraySampleTest()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:12345/");
+            Array arg = new Array();
+            arg.data = new int[] { 1, 2, 3, 4, 5 };
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/Sample/ArraySample", arg);
+            if (response.IsSuccessStatusCode)
+            {
+                Array ret = response.Content.ReadAsAsync<Array>().Result;
+                foreach (int i in ret.data)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static async Task BookSampleGetTest()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:12345/");
+            HttpResponseMessage response = await client.GetAsync("api/Sample/BookSampleGet?author=Дуглас Адамс&title=Автостопом по галактике");
+            if (response.IsSuccessStatusCode)
+            {
+                Book ret = response.Content.ReadAsAsync<Book>().Result;
+                Console.WriteLine(ret.Author + ": " + ret.Title);
+            }
+        }
+
+        static async Task BookSamplePostTest()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:12345/");
+            Book arg = new Book();
+            arg.Author = "Михаил Булгаков";
+            arg.Title = "Мастер и Маргарита";
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/Sample/BookSamplePost", arg);
+            if (response.IsSuccessStatusCode)
+            {
+                Book ret = response.Content.ReadAsAsync<Book>().Result;
+                Console.WriteLine(ret.Author + ": " + ret.Title);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ org.tsu.famc.ds.REST.Server/org/tsu/famc/ds/REST/RESTServer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Owin;
+using Microsoft.Owin.Hosting;
+using System.Runtime.Serialization;
+using System.Web.Http.Cors;
+using System.Net.Http.Formatting;
+
+namespace org.tsu.famc.ds.REST
+{
+
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }        
+    }
+
+    public class Array
+    {
+        public int[] data;
+    }
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class SampleController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult ArraySample(Array arg)
+        {
+            for (int i = 0; i < arg.data.Length/2; i++)
+            {
+                arg.data[i] ^= arg.data[arg.data.Length - i - 1];
+                arg.data[arg.data.Length - i - 1] ^= arg.data[i];
+                arg.data[i] ^= arg.data[arg.data.Length - i - 1];
+            }
+            return Json<Array>(arg);
+        }
+
+        [HttpGet]
+        public IHttpActionResult BookSampleGet(string author, string title)
+        {
+            Book book = new Book();
+            book.Author = author;
+            book.Title = title;
+            return Json<Book>(book);
+        }
+
+        [HttpPost]
+        public IHttpActionResult BookSamplePost(Book book)
+        {
+            Book ret = new Book();
+            ret.Author= book.Author + " (copy)";
+            ret.Title = book.Title + " (copy)";
+            return Json<Book>(ret);
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.EnableCors();
+            config.Routes.MapHttpRoute(
+              "Api", "api/{controller}/{action}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+            app.UseWebApi(config);
+        }
+    }
+
+    public class RESTServer
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://localhost:12345/";
+
+            try
+            {
+                WebApp.Start<Startup>(baseUrl);
+                Console.WriteLine("Started, Press any key to stop.");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
